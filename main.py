8d5ef2e9b9229a68cb445e3a71f9b4c73f84b1a6
Evgenii <evgenii.kravchenko23@mail.ru>
2021-07-19 17:28:54 +0300

    game and counter

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from game_of_life import GameOfLife
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index() :
+def index():
+    GameOfLife(20, 20)
     return render_template('index.html')
 
-if __name__ == '__main__' :
+
+@app.route("/live")
+def live():
+    game = GameOfLife()
+    game.form_new_generation()
+    return render_template("live.html", game=game)
+
+
+if __name__ == '__main__':
     app.run()
